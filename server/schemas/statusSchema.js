29a975c33236e19aa7e982433f1c781d6ef673fa
Qamar Ul Islam <qamarislam1232@gmail.com>
2024-10-14 00:04:49 +0500

    status schema udated

--- server/schemas/statusSchema.js
+++ server/schemas/statusSchema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const statusSchema = Joi.object({
-    status: Joi.string().valid('approved','disapproved','pending').required(),
+    status: Joi.string().valid('approved','disapproved').required(),
     description: Joi.string().required()
 });
 
