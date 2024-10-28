0515d0f4dc49f36dfd0d52cb1062aaecf2854637
Qamar Ul Islam <qamarislam1232@gmail.com>
2024-10-28 19:05:50 +0500

    minor update in repo

--- server/repositories/assignmentRepository.js
+++ server/repositories/assignmentRepository.js
@@ -12,7 +12,7 @@ const getAssignments = async (courseId) => {
 const getAssignmentById = async (assignmentId) => {
   return await Assignment.findById(assignmentId).populate({
     path: 'submissions',
-    select: '_id student',
+    select: '_id student document_id remarks',
     populate: {
       path: 'student',
       select: '_id name',
