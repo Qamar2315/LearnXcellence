241d2da21b8395205ef8ed5413651abb2b9ffe9d
Qamar Ul Islam <qamarislam1232@gmail.com>
2024-11-21 00:01:21 +0500

    added method in question repositoty: getQuestionByIsd

--- server/repositories/questionRepository.js
+++ server/repositories/questionRepository.js
@@ -1,16 +1,21 @@
-const Question = require('../models/Question');
+const Question = require("../models/Question");
 
 // Find a question by its content
 const findQuestionByContent = async (content) => {
-    return await Question.findOne({ content });
+  return await Question.findOne({ content });
 };
 
 // Create a new question
 const createQuestion = async (questionData) => {
-    return await Question.create(questionData);
+  return await Question.create(questionData);
+};
+
+const getQuestionById = async (id) => {
+  return await Question.findById(id);
 };
 
 module.exports = {
-    findQuestionByContent,
-    createQuestion,
+  findQuestionByContent,
+  createQuestion,
+  getQuestionById,
 };
